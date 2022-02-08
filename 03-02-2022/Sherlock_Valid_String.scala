a23a0430d590a9ae5964a77c9df651ff87c99479
Divya-d252 <98879211+Divya-d252@users.noreply.github.com>
2022-02-08 18:20:46 +0530

    Add files via upload

---
+++ 03-02-2022/Sherlock_Valid_String.scala
@@ -0,0 +1,62 @@
+import scala.collection.mutable.Map
+object Sherlock_Valid_String {
+  def main(args: Array[String]): Unit = {
+    println(isValid("abcdefghhgfedecba"))
+  }
+  def isValid(s: String): String = {
+    // Write your code here
+
+    var character_map=Map[Char,Int]()
+    for(i<-0 to s.length-1){
+      if(character_map.contains(s(i))){
+        character_map(s(i))+=1
+      }
+      else{
+        character_map(s(i))=1
+      }
+    }
+
+    //println(character_map.valuesIterator.)
+    var valid_String_map=Map[Int,Int]()
+    for(i<-character_map.values){
+      if(valid_String_map.contains(i)){
+        valid_String_map(i)+=1
+      }
+      else{
+        valid_String_map(i)=1
+      }
+      //println(i+" "+character_map(i))
+    }
+    for(i<-valid_String_map.keys){
+      println(i+" "+valid_String_map(i))
+    }
+    println(valid_String_map.size)
+    if(valid_String_map.size==1){
+      "YES"
+    }
+    else if(valid_String_map.size>2){
+      "NO"
+    }
+    else{
+      var min_key_holder=0
+      var max_key_holder=0
+      for(i<-valid_String_map.keys){
+        if(valid_String_map(i)==valid_String_map.valuesIterator.max)
+          max_key_holder=i
+        else
+          min_key_holder=i
+      }
+      if(valid_String_map.valuesIterator.min==1){
+        if(min_key_holder==max_key_holder+1 | min_key_holder==1){
+          "YES"
+        }
+        else{
+          "NO"
+        }
+      }
+      else{
+        "NO"
+      }
+    }
+  }
+}
